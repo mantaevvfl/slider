59378a96456c4df9f4d1bc8ed59c147f31520660
Ali Mantaev <mantaev.vfl@gmail.com>
2022-04-03 00:26:06 -0400

    Initial Commit

---
+++ Slider/SliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderApp.swift
+//  Slider
+//
+//  Created by Ali Mantaev on 2022-04-03.
+//
+
+import SwiftUI
+
+@main
+struct SliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
